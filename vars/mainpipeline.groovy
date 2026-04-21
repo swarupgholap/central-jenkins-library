da88a7206462d40785577458c9bd73be0e4eba88
swarupgholap <swarupgholap9696@gmail.com>
2026-04-21 14:51:19 +0530

    Update mainpipeline.groovy

--- vars/mainpipeline.groovy
+++ vars/mainpipeline.groovy
@@ -1,11 +1,27 @@
-def call(Map config) {
+def call(String appName) {    
     pipeline {
         agent any
         stages {
-            stage('Download Code') { steps { checkout scm } }
-            stage('Build App') { steps { echo "Building ${config.appName}..." } }
-            stage('Security Scan') { steps { echo "Scanning ${config.appName} for bugs..." } }
-            stage('Deploy') { steps { echo "Deploying to Production..." } }
+            stage('Checkout') {
+                steps {
+                    checkout scm
+                }
+            }
+            stage('Build') {
+                steps {
+                    echo "Building Application: ${appName}"
+                }
+            }
+            stage('Test') {
+                steps {
+                    echo "Testing Application: ${appName}"
+                }
+            }
+            stage('Deploy') {
+                steps {
+                    echo "Deploying ${appName} to Production Server..."
+                }
+            }
         }
     }
 }
